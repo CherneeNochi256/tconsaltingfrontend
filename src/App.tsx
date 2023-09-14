8c4dff9d95bb9513eb0b08d6c5a03ac70d95a266
Максим <maks3d47@gmail.com>
2023-09-14 16:41:52 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from "axios";
 
 function App() {
+
+  const [input, setInput] = useState<string | undefined>()
+  const [response, setResponse] = useState<string | undefined>()
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.get<string>("https://tconsaltingbackend.onrender.com/api/v1/letters/amount", {
+        params: {str: input}
+      })
+      setResponse(response.data);
+    } catch (error: unknown) {
+      console.log(error)
+      alert("Oops... something went wrong")
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", marginTop:300}}>
+        <form onSubmit={handleSubmit} >
+          <input
+              type="text"
+              placeholder="Write something..."
+              value={input === "" ? "" : input}
+              onChange={(e) => setInput(e.target.value)}
+              required
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <div style={{marginTop:15}}>
+          {response}
+        </div>
+      </div>
   );
 }
 
